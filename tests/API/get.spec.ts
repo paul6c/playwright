1f314d310d040b0753b4510708241a7a2d20a987
paul6c <immabeas666@gmail.com>
2026-04-08 22:43:52 +0800

    added get spec

--- tests/API/get.spec.ts
+++ tests/API/get.spec.ts
@@ -19,3 +19,40 @@ test('GET list of users', async ({ request }) => {
     const responseBody = await response.json();
     console.log(responseBody);
 });
+
+test("GET single user", async ({ request }) => {
+    const  USER_ID = 2;
+    const response = await request.get('/api/users/'+ USER_ID);
+    console.log(response.url());
+    console.log(response.status());
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(responseBody.data.id).toBe(USER_ID);
+    expect(response.status()).toBe(200);
+    expect(responseBody.data).toHaveProperty('email');
+    expect(responseBody.data).toHaveProperty('first_name');
+    expect(responseBody.data).toHaveProperty('last_name');
+    expect(responseBody.data).toHaveProperty('avatar');
+});
+
+test("GET single user not found", async ({ request }) => {
+    const  USER_ID = 23;
+    const response = await request.get('/api/users/'+ USER_ID);
+    console.log(response.url());
+    console.log(response.status());
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(response.status()).toBe(404);
+    expect(responseBody).toEqual({});
+});
+
+test("GET list of products", async ({ request }) => {
+    const response = await request.get('/api/products');
+    console.log(response.url());
+    console.log(response.status());
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(response.status()).toBe(200);
+    expect(responseBody).toHaveProperty('data');
+    expect(Array.isArray(responseBody.data)).toBeTruthy();
+});
